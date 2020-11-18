e30acdef22519b9b5d3e35faac44a16cb95fff11
tharcissie <tharcissieidufashe@gmail.com>
2020-11-18 11:51:57 +0200

    test of source

--- tests/news_test.py
+++ tests/news_test.py
@@ -1,23 +1,20 @@
 import unittest
-from models import Source, News
+from app.models import News
 
-
-class SourceTest(unittest.TestCase):
+class NewsTest(unittest.TestCase):
     '''
-    Test Class to test the behaviour of the source class
+    Test Class to test the behaviour of the news class
     '''
 
     def setUp(self):
         '''
         Set up method that will run before every Test
         '''
-        self.new_news = Source(1234,'Python Must Be Crazy','A thrilling new Python Series','https://image.tmdb.org/t/p/w500/khsjha27hbs','New')
+        self.new_news = News(1234,'Python Must Be Crazy','A thrilling new Python Series','news','newws','https://image.tmdb.org/t/p/w500/khsjha27hbs','https://image.tmdb.org/t/p/w500/khsjha27hbs')
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.new_news,Source))
-
-
-
+        self.assertTrue(isinstance(self.new_news,News))
 
 if __name__ == '__main__':
     unittest.main()
+    
