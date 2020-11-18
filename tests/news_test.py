c6246d0bc8b79f2d1c33f0768c16d0da66717ef7
tharcissie <tharcissieidufashe@gmail.com>
2020-11-18 11:37:00 +0200

    request

---
+++ tests/news_test.py
@@ -0,0 +1,23 @@
+import unittest
+from models import Source, News
+
+
+class SourceTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the source class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_news = Source(1234,'Python Must Be Crazy','A thrilling new Python Series','https://image.tmdb.org/t/p/w500/khsjha27hbs','New')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_news,Source))
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
